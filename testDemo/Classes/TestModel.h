b5bf314909ea828ce2bcd7d07c8c08a824dba837
zhouxingbo <973984457@qq.com>
2020-05-08 16:28:37 +0800

    #

---
+++ testDemo/Classes/TestModel.h
@@ -0,0 +1,17 @@
+//
+//  TestModel.h
+//  1234Test
+//
+//  Created by apple on 2020/5/8.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testDemo/Classes/TestModel.m
@@ -0,0 +1,13 @@
+//
+//  TestModel.m
+//  1234Test
+//
+//  Created by apple on 2020/5/8.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import "TestModel.h"
+
+@implementation TestModel
+
+@end
